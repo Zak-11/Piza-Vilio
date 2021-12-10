763d30659b112538836c08e7a8ea924c2a87a702
Zak-11 <dianazakov@rambler.ru>
2021-12-10 20:23:41 +0300

    'finish'

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from "./redux/store";
 import { Provider } from 'react-redux'
-
+import App from "./App";
 import './scss/app.scss'
-import App from './App';
+
+
 
 
 
